53a95c44860ed0c8bd38f053dd0b30a38721e5fd
bijayshrestha0817 <bijay.shrestha2@maitriservices.com>
2026-04-07 05:30:49 +0000

    refactored folder sturcture

--- student_management/apps.py
+++ student_management/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class MyappConfig(AppConfig):
-    name = 'myapp'
+class StudentManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'student_management'
