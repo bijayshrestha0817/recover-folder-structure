9d65d27b7806c0b03a8a586a947ad6a185a33629
bijayshrestha0817 <bijay.shrestha2@maitriservices.com>
2026-04-24 07:17:20 +0000

    refctor custom message handling

--- student_management/repository/student_repository.py
+++ student_management/repository/student_repository.py
@@ -7,3 +7,12 @@ class StudentRepository:
 
     def create_student(self, data):
         return Student.objects.create(**data)
+
+    def update_student(self, student, data):
+        for key, value in data.items():
+            setattr(student, key, value)
+        student.save()
+        return student
+
+    def delete_student(self, student):
+        student.delete()
--- student_management/v1/services/student_service.py
+++ student_management/v1/services/student_service.py
@@ -1,11 +1,41 @@
+from rest_framework import status
+
+from student_management.custom.custom_api_exception import CustomException
+from student_management.models import Student
 from student_management.repository.student_repository import StudentRepository
 
 
 class StudentService:
     def list_students(self):
         repo = StudentRepository()
-        return repo.get_all_students()
+        student = repo.get_all_students()
+        if not student.exists():
+            raise CustomException(message="No Student Found", status_code=status.HTTP_404_NOT_FOUND)
+        return student
 
     def create_student(self, validated_data):
         repo = StudentRepository()
+        if Student.objects.filter(email=validated_data["email"]):
+            raise CustomException(
+                message="Student with this email already exits",
+                status_code=status.HTTP_409_CONFLICT,
+            )
         return repo.create_student(validated_data)
+
+    def update_student(self, student, validated_data):
+        repo = StudentRepository()
+        if (
+            "email" in validated_data
+            and Student.objects.filter(email=validated_data["email"])
+            .exclude(id=student.id)
+            .exists()
+        ):
+            raise CustomException(
+                message="Student with this email already exists",
+                status_code=status.HTTP_409_CONFLICT,
+            )
+        return repo.update_student(student, validated_data)
+
+    def delete_student(self, student):
+        repo = StudentRepository()
+        return repo.delete_student(student)
