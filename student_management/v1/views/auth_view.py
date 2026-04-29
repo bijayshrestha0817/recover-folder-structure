26cf7793dba65db5193ca87fd5ea786ca37045f6
bijayshrestha0817 <bijay.shrestha2@maitriservices.com>
2026-04-29 11:57:53 +0000

    change password integration done...

--- student_management/v1/views/auth_view.py
+++ student_management/v1/views/auth_view.py
@@ -1,7 +1,6 @@
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 
-from student_management.custom.custom_api_exception import CustomException
 from student_management.custom.custom_response import CustomResponse
 from student_management.v1.serializers.auth_serializer import ChangePasswordSerializer
 
@@ -12,13 +11,10 @@ class ChangePasswordView(generics.UpdateAPIView):
     def put(self, request):
         serializer = ChangePasswordSerializer(data=request.data, context={"request": request})
 
-        if serializer.is_valid():
-            serializer.save()
-            return CustomResponse(
-                message="Password changed successfully!",
-                status=status.HTTP_200_OK,
-            )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        raise CustomException(
-            message=str(serializer.errors), status_code=status.HTTP_400_BAD_REQUEST
+        return CustomResponse(
+            message="Password changed successfully!",
+            status=status.HTTP_200_OK,
         )
